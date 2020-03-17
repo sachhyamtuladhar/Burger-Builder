eab24ad46a7eedabbffc58d51d695d37b10c3e94
sachhyam <tsachhyam@gmail.com>
2020-03-17 17:32:21 +0545

    Wrap UP

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,28 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, Component} from 'react'
 import styles from './Modal.module.scss'
 import BackDrop from '../BackDrop/BackDrop'
 
-const Modal = props => {
-    return (
-        <Fragment>
-            <BackDrop show={props.show} clicked={props.close}/>
-            <div 
-                className = {styles.Modal}
-                style = {{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}
-            >
-                {props.children}
-            </div>
-        </Fragment>
-    )
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show
+    }
+
+    render(){
+        return (
+            <Fragment>
+                <BackDrop show={this.props.show} clicked={this.props.close}/>
+                <div 
+                    className = {styles.Modal}
+                    style = {{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Fragment>
+        )
+    }
 }
 
 export default Modal
